6b89d03dd36e1c7df8e94687d797abcf3ff2c0a0
ArthDh <arth.dharaskar97@gmail.com>
2020-12-10 11:57:20 -0800

    Added Final version of Artist recommendation + minor cleanup

--- util/spotify_interface.py
+++ util/spotify_interface.py
@@ -3,7 +3,11 @@ from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
 import pickle as pkl
 import requests
-
+import numpy as np
+import pandas as pd
+import copy
+from pathlib import Path
+from IPython.display import display,HTML,clear_output
 
 def get_spotify_token(username, scope, redirect_url, client_id, client_secret):
     '''
@@ -128,3 +132,85 @@ def get_recommendations(artists, sp, lim = 1):
                        seed_tracks=pred_artist_col.seed_tracks, limit=lim))
     
     return recommendations
+
+
+def get_artists_rec(artist_name='Drake', lim=5):
+    '''
+        Returns a List of recommendation which can be parsed in the notebook
+        
+        artist_name: Name of artist to recommend based on 
+        lim: Number of recommendations to return
+    '''
+    assert isinstance(artist_name, str), "Name should be a string"
+    assert isinstance(lim, int), "Limi should be a integer"
+    
+    
+    data_path = Path('../data/')
+    model_path = Path('../model/')
+    df_f_path= data_path / Path('data.csv')
+    df_f = pd.read_csv(df_f_path)
+    df_f = df_f.drop(['artists', 'id', 'name', 'release_date', 'year', 'explicit'], axis=1)
+    
+    df_min = df_f.min()
+    df_max = df_f.max()
+    
+    path = '../data/df_cleaned__by_artist.csv'
+    df = pd.read_csv(path)
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df = df.drop(['explicit'], axis=1)
+    df = df[~df.duplicated()==1]
+    X = copy.deepcopy(df.drop(labels = ['artists'],axis = 1))
+    arti_names = df.artists.to_numpy()
+    
+    with open('../util/spotify_client.txt', 'rb') as f:
+        (client_id, client_secret, oauth_token) = pkl.load(f)
+    
+
+    scope = "user-library-read"
+    username = "Arth"
+    redirect_url = 'http://127.0.0.1:9090'
+    
+    
+    sp = get_spotify_token(username, scope, redirect_url, client_id,client_secret)
+    
+    features =  ['acousticness',
+                 'danceability',
+                 'duration_ms',
+                 'energy',
+                 'explicit',
+                 'instrumentalness',
+                 'key',
+                 'liveness',
+                 'loudness',
+                 'mode',
+                 'popularity',
+                 'speechiness',
+                 'tempo',
+                 'valence']
+    
+    artist = get_artist([artist_name], sp)
+    artist_dict = get_features(artist[0], sp, features)
+    artist_series = pd.Series(artist_dict)
+    
+    artist_scaled = (artist_series-df_min) / (df_max - df_min)
+    
+    rank = np.sum((X - artist_scaled)**2,axis=1)
+    idx = np.argpartition(rank, 5)
+    rec = get_recommendations(arti_names [idx][:1], sp, lim= lim)
+    
+    render_rec(rec, lim)
+    return rec
+
+def render_rec(rec, lim):
+    '''
+        Renders the recommendation List in a Jupyter notebook
+        
+        rec: recommendation List
+    '''
+    assert isinstance(rec, list), 'Recommendations should be in the form of list'
+    
+    display(HTML("<hr></hr>"))
+    for i in range(0, lim):
+        display(HTML(f"<h1> <b>Artist:</b> {rec[0]['tracks'][i]['artists'][0]['name']}\t </h1> <h2> <b>Album:</b> {rec[0]['tracks'][i]['album']['name']} </h2> <br> <img src='{rec[0]['tracks'][i]['album']['images'][1]['url']}'> <br> <audio controls><source src='{rec[0]['tracks'][i]['preview_url']}' type='audio/ogg'> </audio>"))
+        display(HTML("<hr></hr>"))
+
